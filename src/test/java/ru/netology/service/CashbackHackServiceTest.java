a06fb6b0dd576f02feafe891e5795a162b46aa50
Dmitry Kovtun <mr_write@mail.ru>
2022-03-16 16:15:15 +0300

    junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,53 @@
-import static org.testng.Assert.*;
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CashbackHackServiceTest {
-    
+
+    @Test
+    public void souldTestRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amout = 800;
+        int expected = 200;
+        int actual = service.remain(amout);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void souldTestRemainExpected1() {
+        CashbackHackService service = new CashbackHackService();
+        int amout = 1999;
+        int expected = 1;
+        int actual = service.remain(amout);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void souldTestRemainExpected999() {
+        CashbackHackService service = new CashbackHackService();
+        int amout = 1;
+        int expected = 999;
+        int actual = service.remain(amout);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void souldTestRemainExpected1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amout = 0;
+        int expected = 1000;
+        int actual = service.remain(amout);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void souldTestRemainElseExpected1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amout = 1000;
+        int expected = 1000;
+        int actual = service.remain(amout);
+        assertEquals(actual, expected);
+    }
 }
